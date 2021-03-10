8da2e4d96e393fafe2f5b4390ef84192dd08cc06
Sumati30 <sumati.khed30@gmail.com>
2021-03-09 17:00:26 -0800

    NFA

---
+++ external.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    document.getElementById('try').innerHTML = "THis Is The Exaple Of External JavaScript";
+}
+
+function myImage(){
+    document.getElementById('pic').src = '../Images/fall.gif';
+}
